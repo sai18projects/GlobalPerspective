9317bf6f9e6b750e6a3c11d263fec9fa9fb814e1
sai18projects <sai18projetcs@gmail.com>
2021-12-24 19:42:23 +0530

    1 file modified

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var value = document.getElementById('content').value;
 var title = document.getElementById('title');
 var maincontext = document.getElementById('main-content');
 function content() {
-
-  if (value = 'TRR') {
-    title.innerHTML = "Team Research Report";
+  if (value == 'TRR') {
+    console.log('hi');
+    title.textContent = 'Team Research Report';
     // maincontext.innerHTML = 'test!';
   }
 }
