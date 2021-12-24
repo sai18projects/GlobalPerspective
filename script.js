ac015741d55156441cc6ffe5f2d903214da83872
sai18projects <sai18projetcs@gmail.com>
2021-12-24 20:31:18 +0530

    2 files modified

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var value = document.getElementById('content').value;
 var title = document.getElementById('title');
+
 var maincontext = document.getElementById('main-content');
+var gp =
+  'Global Perspectives is a subject in which  students at education develop outstanding transferable skills, including critical thinking, research and collaboration. It is fun place in which where do research and have fun. For 6th Grade we did reseach on the continet called Ocienia, Induvidual Research Report, Team Research Report and many more';
+
 function content() {
   var value = document.getElementById('content').value;
   var title = document.getElementById('title');
@@ -11,7 +15,6 @@ function content() {
     maincontext.innerHTML = 'Work in Progress!!';
   } else if (value == 'Home') {
     title.textContent = 'What is Gp?';
-    maincontext.innerHTML =
-      ' Global Perspectives is a subject in which  students at education develop outstanding transferable skills, including critical thinking, research and collaboration. It is fun place in which where do research and have fun. For 6th Grade we did reseach on the continet called Ocienia, Induvidual Research Report, Team Research Report and many more ';
+    maincontext.innerHTML = gp;
   }
 }
