1ce990650af032d843bd19100e72d20902164efd
sai18projects <sai18projetcs@gmail.com>
2021-12-24 21:10:54 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+document.getElementById('hi').style.display = 'none';
 var value = document.getElementById('content').value;
 var title = document.getElementById('title');
 
@@ -13,12 +14,27 @@ function content() {
   if (value == 'TRR') {
     title.textContent = 'Team Research Report';
     maincontext.innerHTML = 'Work in Progress!!';
+    document.getElementById('hi').style.display = 'none';
+
+
+
+
+    
   } else if (value == 'Home') {
     title.textContent = 'What is Global Perspective?';
     maincontext.innerHTML =
       'Global Perspectives is a subject in which  students at education develop outstanding transferable skills, including critical thinking, research and collaboration. It is fun place in which where do research and have fun. For 6th Grade we did reseach on the continet called Ocienia, Induvidual Research Report, Team Research Report and many more';
-  } else if(value == "IRR"){
-    title.textContent = 'Education'
-    maincontext.textContent = "<iframe src="https://docs.google.com/document/d/e/2PACX-1vQc9irotNwh0HGGL6FpVmtrsukXbiGYsX8obn0WeD4a1wH943DBc3xD8UTSxHH95QVz0dgOK7W8BTD_/pub?embedded=true"></iframe>"
+    document.getElementById('hi').style.display = 'none';
+
+
+
+
+
+
+  } else if (value == 'IRR') {
+    title.innerHTML = 'Induvidual Research Report';
+    maincontext.style.display = 'none';
+
+    document.getElementById('hi').style.display = 'block';
   }
 }
