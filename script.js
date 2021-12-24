229bac00cbdde66abf44f0ae0aff1ed05a7065ea
sai18projects <sai18projetcs@gmail.com>
2021-12-24 20:43:50 +0530

    1 file modified

--- script.js
+++ script.js
@@ -14,7 +14,11 @@ function content() {
     title.textContent = 'Team Research Report';
     maincontext.innerHTML = 'Work in Progress!!';
   } else if (value == 'Home') {
-    title.textContent = 'What is Gp?';
-    maincontext.innerHTML = gp;
+    title.textContent = 'What is Global Perspective?';
+    maincontext.innerHTML =
+      'Global Perspectives is a subject in which  students at education develop outstanding transferable skills, including critical thinking, research and collaboration. It is fun place in which where do research and have fun. For 6th Grade we did reseach on the continet called Ocienia, Induvidual Research Report, Team Research Report and many more';
+  } else if(value == "IRR"){
+    title.textContent = 'Education'
+    maincontext.textContent = "<iframe src="https://docs.google.com/document/d/e/2PACX-1vQc9irotNwh0HGGL6FpVmtrsukXbiGYsX8obn0WeD4a1wH943DBc3xD8UTSxHH95QVz0dgOK7W8BTD_/pub?embedded=true"></iframe>"
   }
 }
