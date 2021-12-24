305432b422f635a594935560e90edb328f26fc35
sai18projects <sai18projetcs@gmail.com>
2021-12-24 19:54:48 +0530

    1 file modified

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ var value = document.getElementById('content').value;
 var title = document.getElementById('title');
 var maincontext = document.getElementById('main-content');
 function content() {
+  var value = document.getElementById('content').value;
+  var title = document.getElementById('title');
+  var maincontext = document.getElementById('main-content');
+
   if (value == 'TRR') {
     console.log('hi');
     title.textContent = 'Team Research Report';
-    // maincontext.innerHTML = 'test!';
+    maincontext.innerHTML = 'test!';
   }
 }
